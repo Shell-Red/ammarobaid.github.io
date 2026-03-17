768c3aaa629452ff562ad8d3eef809a5b7615d4c
Shell-Red <ammar.eobeid@gmail.com>
2026-03-17 03:56:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section');
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if(rect.top < window.innerHeight - 100){
+      section.style.opacity = 1;
+      section.style.transform = 'translateY(0)';
+      section.style.transition = 'all 0.8s ease-out';
+    } else {
+      section.style.opacity = 0;
+      section.style.transform = 'translateY(50px)';
+    }
+  });
+});
